3705e8e2bd7d29608f94a179d5fce92c58f19ddf
venshupo <Venshupo@gmail.com>
2021-03-09 00:30:43 +0300

    implement loading and senfing json file Task2

--- couriers/urls.py
+++ couriers/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
-from django.urls import path
 from . import views
-
 
 urlpatterns = [
     url('^$', views.post_couriers),
-    url('(\d+)/$', views.edit_courier)
+    url('^(\d+)$', views.edit_courier)
 ]
--- couriers/views.py
+++ couriers/views.py
@@ -1,6 +1,6 @@
 import json
 from django.http import HttpResponse
-from django.views.decorators.http import require_POST, require_GET
+from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -31,14 +31,32 @@ def post_couriers(request):
 
     # create response
     if not bad_id:
-        response = {"couriers": pretty_id}
+        data_response = {"couriers": pretty_id}
+        status_response = 201
     else:
-        response = {"couriers": bad_id}
+        data_response = {"validation_error": {"couriers": bad_id}}
+        status_response = 400
 
-    return HttpResponse(json.dumps(response), status=201)
+    return HttpResponse(json.dumps(data_response), status=status_response)
 
 
-def edit_courier(request, id):
-    # меняет информацию о курьере с переданным id
-    if request.method == "GET":
-        return HttpResponse(id, status=200)
+@csrf_exempt
+def edit_courier(request, courier_id):
+    """ меняет информацию о курьере с переданным id"""
+    if request.method == "PATCH":
+
+        # read json
+        # new_feature = json.loads(request.body)
+
+        # edit info in db
+        try:
+            # ...some code...
+            data_response = courier_id
+            # ...some code...
+            return HttpResponse(json.dumps(data_response), status=200)
+        except IOError:
+            return HttpResponse(status=400)
+
+
+
+
