56fc65ccf4e76d1cc8b6bc4c1b19692c351d534c
venshupo <Venshupo@gmail.com>
2021-03-08 23:54:20 +0300

    implement loading and sending json file

--- couriers/views.py
+++ couriers/views.py
@@ -1,24 +1,41 @@
 import json
-from django.template import loader
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.views.decorators.http import require_POST, require_GET
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 @require_POST
 def post_couriers(request):
     """
-        из тела запроса вытащить json и заполнить бд
+        из тела запроса вытащить json, заполнить бд
+        и вернуть json
     """
-    # read json begin
-    data = json.loads(request.body)
+    # read json
+    list_couriers = json.loads(request.body)["data"]
 
-    title = "HTTP 201 Created"
+    # prepare list for response
+    bad_id = []
+    pretty_id = []
 
-    # render and create response
-    t = loader.get_template('Couriers_POST.html')
-    context = {'title': title, 'list_created_id': data}
-    return HttpResponse(t.render(context, request), status=200)
+    # fill in db
+    for worker in list_couriers:
+        try:
+            # ...some code...
+            pretty_id.append({'id': worker['courier_id']})
+            pass
+        except IOError:
+            pass
+
+    # ...some code...
+
+    # create response
+    if not bad_id:
+        response = {"couriers": pretty_id}
+    else:
+        response = {"couriers": bad_id}
+
+    return HttpResponse(json.dumps(response), status=201)
 
 
 def edit_courier(request, id):
